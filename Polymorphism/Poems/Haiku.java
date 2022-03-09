d1e98abeec8a6d86533d4dd696f7de90866db074
Jam Cavalida <83889170+maicqxx@users.noreply.github.com>
2022-03-09 10:56:01 +0800

    Add files via upload

---
+++ Polymorphism/Poems/Haiku.java
@@ -0,0 +1,8 @@
+package ProfessionalElectiveOOP.Polymorphism.Poems;
+
+//subclass
+public class Haiku extends Poem{
+    public Haiku(String poemName, int numOfLines){
+        super(poemName, numOfLines);
+    }
+}
